38f4e3397df8c9d3151d6a83125faf5205a6c8d5
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-02-27 18:27:07 -0300

    Terceiro item do Req2 completo.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -35,7 +35,19 @@ describe('Testando o componente <About.js />', () => {
 
   it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.',
     () => {
+      const { history } = renderWithRouter(<App />);
 
+      history.push('/about');
+
+      const text1 = /This application simulates a Pokédex,/
+      + / a digital encyclopedia containing all Pokémons/;
+      const infoP1El = screen.getByRole('paragraph', { name: text1 });
+      const text2 = /One can filter Pokémons by type,/
+      + / and see more details for each one of them/;
+      const infoP2El = screen.getByRole('paragraph', { name: text2 });
+
+      expect(infoP1El).toBeInTheDocument();
+      expect(infoP2El).toBeInTheDocument();
     });
 
   it('Teste se a página contém a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.',
