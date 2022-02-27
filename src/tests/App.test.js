a95017e9fe194899b230403ca6af4741b40c2ba6
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-02-27 16:48:34 -0300

    Requisito 1 completo.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 
 import App from '../App';
-import userEvent from '@testing-library/user-event';
 
 describe('Testando o componente <App.js />', () => {
   it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação.',
@@ -24,9 +24,11 @@ describe('Testando o componente <App.js />', () => {
   () => {
     const { history } = renderWithRouter(<App />);
     const homeEl = screen.getByRole('link', { name: 'Home' });
-    const { pathname } = history.location;
 
     userEvent.click(homeEl);
+
+    const { pathname } = history.location;
+
     expect(pathname).toBe('/');
   });
 
@@ -35,21 +37,34 @@ describe('Testando o componente <App.js />', () => {
   () => {
     const { history } = renderWithRouter(<App />);
     const aboutEl = screen.getByRole('link', { name: 'About' });
-    const { pathname } = history.location;
 
     userEvent.click(aboutEl);
+
+    const { pathname } = history.location;
+
     expect(pathname).toBe('/about');
   });
 
   it(`Teste se a aplicação é redirecionada para a página de Pokémons Favoritados,
    na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação.`,
   () => {
+    const { history } = renderWithRouter(<App />);
+    const favsEl = screen.getByRole('link', { name: 'Favorite Pokémons' });
 
+    userEvent.click(favsEl);
+
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/favorites');
   });
 
   it(`Teste se a aplicação é redirecionada para a página Not Found
    ao entrar em uma URL desconhecida.`,
   () => {
+    const { history } = renderWithRouter(<App />);
 
+    history.push('/pagina/que-nao-existe/');
+
+    expect.stringContaining('Page requested not found');
   });
 });
