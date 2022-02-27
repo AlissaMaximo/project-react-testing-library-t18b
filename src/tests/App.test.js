630823fb7fb8379629ae46943b80c36646cfe2d8
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-02-27 15:17:08 -0300

    Primeiro it do Req1 feito.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,29 +1,43 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+
+import App from '../App';
+
 describe('Testando o componente <App.js />', () => {
   it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação.',
     () => {
+      renderWithRouter(<App />);
 
+      const homeEl = screen.getByRole('link', { name: 'Home' });
+      const aboutEl = screen.getByRole('link', { name: 'About' });
+      const favsEl = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+      expect(homeEl).toBeInTheDocument();
+      expect(aboutEl).toBeInTheDocument();
+      expect(favsEl).toBeInTheDocument();
     });
 
-  it('Teste se a aplicação é redirecionada para a página inicial,'
-  + ' na URL / ao clicar no link Home da barra de navegação.',
+  it(`Teste se a aplicação é redirecionada para a página inicial,
+  na URL / ao clicar no link Home da barra de navegação.`,
   () => {
 
   });
 
-  it('Teste se a aplicação é redirecionada para a página de About,'
-  + ' na URL /about, ao clicar no link About da barra de navegação.',
+  it(`Teste se a aplicação é redirecionada para a página de About,
+   na URL /about, ao clicar no link About da barra de navegação.`,
   () => {
 
   });
 
-  it('Teste se a aplicação é redirecionada para a página de Pokémons Favoritados,'
-  + ' na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação.',
+  it(`Teste se a aplicação é redirecionada para a página de Pokémons Favoritados,
+   na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação.`,
   () => {
 
   });
 
-  it('Teste se a aplicação é redirecionada para a página Not Found'
-  + 'ao entrar em uma URL desconhecida.',
+  it(`Teste se a aplicação é redirecionada para a página Not Found
+   ao entrar em uma URL desconhecida.`,
   () => {
 
   });
