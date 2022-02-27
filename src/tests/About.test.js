05777873ec4726f6991b22c965254a35e5d52d43
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-02-27 17:46:52 -0300

    Primeiro item do Req2 completo.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,7 +1,24 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+
+import App from '../App';
+
 describe('Testando o componente <About.js />', () => {
   it('Teste se a página contém as informações sobre a Pokédex.',
     () => {
+      renderWithRouter(<App />);
+      const { history } = renderWithRouter(<App />);
 
+      history.push('/about');
+
+      const infoP1El = screen.getByText(/This application simulates a Pokédex,/,
+        / a digital encyclopedia containing all Pokémons/);
+      const infoP2El = screen.getByText(/One can filter Pokémons by type,/,
+        / and see more details for each one of them/);
+
+      expect(infoP1El).toBeInTheDocument();
+      expect(infoP2El).toBeInTheDocument();
     });
 
   it('Teste se a página contém um heading h2 com o texto About Pokédex.',
