b03564f9cb9c1746f0097c11741f018e8af71a0d
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-02-27 18:42:42 -0300

    Terceiro item do Requisito 2 completo.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -39,15 +39,16 @@ describe('Testando o componente <About.js />', () => {
 
       history.push('/about');
 
-      const text1 = /This application simulates a Pokédex,/
-      + / a digital encyclopedia containing all Pokémons/;
-      const infoP1El = screen.getByRole('paragraph', { name: text1 });
-      const text2 = /One can filter Pokémons by type,/
-      + / and see more details for each one of them/;
-      const infoP2El = screen.getByRole('paragraph', { name: text2 });
+      const infoP1El = screen.getByText(/This application simulates a Pokédex,/,
+        / a digital encyclopedia containing all Pokémons/); // verifica se tem o texto
+      const infoP2El = screen.getByText(/One can filter Pokémons by type,/,
+        / and see more details for each one of them/); // verifica se tem o segundo texto
+      const wordOccurence = screen.getAllByText(/pokémons/i).length; // a palavra pokémons aparece uma vez no link, uma vez no primeiro parágrafo, e uma vez no segundo. Isto foi feito pq não há Aria role de parágrafo, não há classname no section q contém os parágrafos nem nos parágrafos.
+      const NUMBER_THREE = 3;
 
       expect(infoP1El).toBeInTheDocument();
       expect(infoP2El).toBeInTheDocument();
+      expect(wordOccurence).toBe(NUMBER_THREE);
     });
 
   it('Teste se a página contém a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.',
