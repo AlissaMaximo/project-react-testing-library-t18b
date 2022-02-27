37ce5a607a06fdb0e92ccfbc9c8d490a05e7a866
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-02-27 20:17:43 -0300

    Descrições do Requisito 5 feitas.

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+
+import App from '../App';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  it('Teste se página contém um heading h2 com o texto Encountered pokémons.',
+    () => {
+      renderWithRouter(<App />);
+    });
+
+  it(`Teste se é exibido o próximo Pokémon da lista
+   quando o botão Próximo pokémon é clicado.`,
+  () => {
+    renderWithRouter(<App />);
+  });
+
+  it('Teste se é mostrado apenas um Pokémon por vez.',
+    () => {
+      renderWithRouter(<App />);
+    });
+
+  it('Teste se a Pokédex tem os botões de filtro.',
+    () => {
+      renderWithRouter(<App />);
+    });
+
+  it('Teste se a Pokédex contém um botão para resetar o filtro.',
+    () => {
+      renderWithRouter(<App />);
+    });
+});
