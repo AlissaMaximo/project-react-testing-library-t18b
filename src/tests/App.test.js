7c1ff615005a32a8763c1207d96a5816b1b8a0cb
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-02-27 16:09:59 -0300

    Terceiro item do Req1 feito.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,6 +3,7 @@ import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 
 import App from '../App';
+import userEvent from '@testing-library/user-event';
 
 describe('Testando o componente <App.js />', () => {
   it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação.',
@@ -21,13 +22,23 @@ describe('Testando o componente <App.js />', () => {
   it(`Teste se a aplicação é redirecionada para a página inicial,
   na URL / ao clicar no link Home da barra de navegação.`,
   () => {
+    const { history } = renderWithRouter(<App />);
+    const homeEl = screen.getByRole('link', { name: 'Home' });
+    const { pathname } = history.location;
 
+    userEvent.click(homeEl);
+    expect(pathname).toBe('/');
   });
 
   it(`Teste se a aplicação é redirecionada para a página de About,
    na URL /about, ao clicar no link About da barra de navegação.`,
   () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutEl = screen.getByRole('link', { name: 'About' });
+    const { pathname } = history.location;
 
+    userEvent.click(aboutEl);
+    expect(pathname).toBe('/about');
   });
 
   it(`Teste se a aplicação é redirecionada para a página de Pokémons Favoritados,
