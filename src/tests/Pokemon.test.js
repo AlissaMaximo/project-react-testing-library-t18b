ec96a319b7e6e06965d2a4fd0ba6a19ef56eb31f
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-02-28 03:26:45 -0300

    Requisito 6 completo.

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 /* import userEvent from '@testing-library/user-event';
- */import renderWithRouter from '../renderWithRouter';
+ */import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
 
 import App from '../App';
 
 describe('Teste o componente <Pokemon.js />', () => {
+  const pikachuURL = '/pokemons/25';
+
   it('Teste se é renderizado um card com as informações de determinado pokémon.',
     () => {
       renderWithRouter(<App />);
@@ -26,7 +29,7 @@ describe('Teste o componente <Pokemon.js />', () => {
   () => {
     renderWithRouter(<App />);
     const pikachuLink = screen.getByRole('link', {
-      name: 'More details', src: '/pokemons/25' });
+      name: 'More details', src: pikachuURL });
 
     expect(pikachuLink).toBeInTheDocument();
   });
@@ -34,17 +37,31 @@ describe('Teste o componente <Pokemon.js />', () => {
   it(`Teste se ao clicar no link de navegação do Pokémon,
    é feito o redirecionamento da aplicação para a página de detalhes de Pokémon.`,
   () => {
-    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
+    const pikachuLink = screen.getByRole('link', {
+      name: 'More details' });
+
+    userEvent.click(pikachuLink);
+
+    const { pathname } = history.location;
+
+    expect(pathname).toBe(pikachuURL);
   });
 
-  it(`Teste também se a URL exibida no navegador muda para
-   /pokemon/<id>, onde <id> é o id do Pokémon cujos detalhes se deseja ver;`,
-  () => {
-    renderWithRouter(<App />);
-  });
+  // O quarto item entra no terceiro.
 
   it('Teste se existe um ícone de estrela nos Pokémons favoritados.',
     () => {
-      renderWithRouter(<App />);
+      const { history } = renderWithRouter(<App />);
+
+      history.push(pikachuURL);
+
+      const favCheckLabel = screen.getByLabelText('Pokémon favoritado?');
+
+      userEvent.click(favCheckLabel);
+
+      const starIcon = screen.getByAltText('Pikachu is marked as favorite');
+
+      expect(starIcon).toHaveAttribute('src', '/star-icon.svg');
     });
 });
